c353ae63c533b33b91d5ec4741e828ee085028e3
Lukas Troyer <36337818+lukas-tr@users.noreply.github.com>
2019-10-23 18:46:47 +0200

    add travel planner

---
+++ TravelPlannerApi/TravelPlanner/Model.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TravelPlanner
+{
+    public class Trip
+    {
+        public string Leave { get; set; }
+        public string Arrive { get; set; }
+    }
+
+    public class Route
+    {
+        public string City { get; set; }
+        public Trip[] ToLinz { get; set; }
+        public Trip[] FromLinz { get; set; }
+    }
+
+    public class TripWithLocations : Trip
+    {
+        public string FromCity { get; set; }
+        public string ToCity { get; set; }
+    }
+}
---
+++ TravelPlannerApi/TravelPlannerApi/Controllers/TravelPlanController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using TravelPlanner;
+
+namespace TravelPlannerApi.Controllers
+{
+    [ApiController]
+    [Route("api/travelPlan")]
+    public class TravelPlanController : ControllerBase
+    {
+        private readonly ILogger<TravelPlanController> _logger;
+        private readonly IHttpClientFactory _factory;
+
+        public TravelPlanController(ILogger<TravelPlanController> logger, IHttpClientFactory factory)
+        {
+            _logger = logger;
+            _factory = factory;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get([FromQuery] string from, [FromQuery] string to, [FromQuery] string start)
+        {
+            var client = _factory.CreateClient();
+            var resp = await client.GetAsync("https://cddataexchange.blob.core.windows.net/data-exchange/htl-homework/travelPlan.json");
+            resp.EnsureSuccessStatusCode();
+            var routes = JsonSerializer.Deserialize<Route[]>(await resp.Content.ReadAsStringAsync());
+            var finder = new ConnectionFinder(routes);
+            var trip = finder.FindConnection(from, to, start);
+            if (trip == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(new { 
+                depart = trip.FromCity,
+                departureTime = trip.Leave,
+                arrive = trip.ToCity,
+                arrivalTime = trip.Arrive,
+            });
+        }
+    }
+}
